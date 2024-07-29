62ae3a889f1cdcc9826e7399473dd75cd1d1d4b4
Santiago <tabordasantiago350@gmail.com>
2024-07-29 01:23:01 -0500

    Quinto ejercicio

---
+++ src/interactivo/SegundoEjercicio.java
@@ -0,0 +1,17 @@
+package interactivo;
+
+public class SegundoEjercicio {
+	
+	    public static void main(String[] args) {
+	        int i = 1;
+	        int suma = 0; 
+   
+	        while (i <= 10) {
+	            suma += i;
+	            i++; 
+	        }
+	     
+	        System.out.println("La suma de los números del 1 al 10 es: " + suma);
+	    }
+
+}
---
+++ src/interactivo/TercerEjercicio.java
@@ -0,0 +1,15 @@
+package interactivo;
+
+public class TercerEjercicio {
+	
+	    public static void main(String[] args) {
+	        int i = 10; 
+	      
+	        while (i >= 1) {
+	            System.out.println(i);
+	            i--;
+	        }
+	    }
+	
+
+}
